b6d2fcb7eea43272751678af225b5749d9f5733d
GardnerA4 <116819763+GardnerA4@users.noreply.github.com>
2025-07-30 13:46:47 -0400

    roaming logic updated

--- Assets/Scripts/Monster/AIVison.cs
+++ Assets/Scripts/Monster/AIVison.cs
@@ -1,21 +1,24 @@
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class AIVision : MonoBehaviour
 {
     public Transform player;
     public float visionRange = 15f;
     public float viewAngle = 90f;
-    public LayerMask obstacleMask;
+    public LayerMask obstacleMask; 
 
     public bool CanSeePlayer()
     {
-        Vector3 dirToPlayer = (player.position - transform.position).normalized;
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if (Vector3.Distance(transform.position, player.position) < visionRange)
+        // Check if player is in vision range and angle
+        if (distanceToPlayer < visionRange)
         {
-            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2f)
+            if (Vector3.Angle(transform.forward, directionToPlayer) < viewAngle / 2f)
             {
+
                 if (!Physics.Linecast(transform.position, player.position, obstacleMask))
                 {
                     return true;
